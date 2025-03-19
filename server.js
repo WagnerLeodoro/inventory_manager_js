2def3fb1d0662535117d111066e1ca58a6a8f03f
Instrutor-WagnerLeodoro <wagner.leodoro@docente.pr.senac.br>
2025-03-19 10:33:00 -0300

    🐛 :fix ajuste nos imports

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { appRoutes } = require('./src/routes');
+const appRoutes = require('./src/routes');
 
 const app = express();
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { produtoRoutes } = require("./produto.routes.js");
+const produtoRoutes = require("./produto.routes.js");
 
 const appRoutes = express.Router();
 
 appRoutes.use("/produtos", produtoRoutes)
 
-module.exports = { appRoutes };
+module.exports = appRoutes;
