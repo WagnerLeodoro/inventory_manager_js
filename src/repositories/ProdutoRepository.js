fca1ed2158db71cc3c114632408af081401f3214
Instrutor-WagnerLeodoro <wagner.leodoro@docente.pr.senac.br>
2025-03-19 10:37:30 -0300

    ✨:feat - inclusão de arrays de objetos em data.json

--- src/repositories/ProdutoRepository.js
+++ src/repositories/ProdutoRepository.js
@@ -1,81 +1,50 @@
 const Produto = require("../model/Produto");
-const fs = require('node:fs');
-const path = require('node:path');
 
 class ProdutoRepository {
     constructor(dataService) {
         this.dataService = dataService;
+        this.produtos = this.dataService.data.produtos;
     }
 
     // Listar todos os produtos
     listarProdutos() {
-        return this.dataService.produtos;
+        return this.produtos;
     }
 
     // Adicionar um novo produto
     adicionarProduto({nome, preco}) {
         const produto = new Produto(nome, preco);
-        this.dataService.produtos.push(produto);
-        this.dataService.salvarDados(this.dataService.produtos);
+        this.produtos.push(produto);
+        this.dataService.data.produtos = this.produtos;
+        this.dataService.salvarDados(this.dataService.data);
     }
 
     // Remover um produto pelo ID
     removerProduto(id) {
-        let produtos = this.dataService.produtos.findIndex(produto => produto.id === id);
+        let produtos = this.produtos.findIndex(produto => produto.id === id);
         if (produtos === -1) {
             throw new Error("Produto não encontrado!");
         } else {
-            this.dataService.produtos.splice(produtos, 1);
+            this.produtos.splice(produtos, 1);
         }        
-        this.dataService.salvarDados(this.dataService.produtos);
+        this.dataService.salvarDados(this.produtos);
     }
 
     // Atualizar um produto pelo ID
     atualizarProduto(id, produtoAtualizado) {
-        this.dataService.produtos = this.dataService.produtos.map(produto => {
+        this.produtos = this.produtos.map(produto => {
             if (produto.id === id) {
 
                 return produtoAtualizado;
             }
             return produto;
         });
-        this.dataService.salvarDados(this.dataService.produtos);
+        this.dataService.salvarDados(this.produtos);
     }
 
     // Buscar um produto pelo ID
     buscarProduto(id) {
-        return this.dataService.produtos.find(produto => produto.id === id);
-    }
-
-    // Gerar relatório de produtos
-    gerarRelatorio() {
-        const produtos = this.dataService.produtos;
-        const totalGeral = produtos.reduce((total, produto) => total + produto.preco, 0);
-        const totalProdutos = produtos.length;
-
-        let csv = 'Nome,Preço\n';
-        produtos.forEach(produto => {
-            csv += `${produto.nome},${produto.preco}\n`;
-        });
-        csv += `Total de produtos: ${totalProdutos}\n`;
-        csv += `Total geral: ${totalGeral}\n`;
-
-        const dirPath = path.join(__dirname, '../relatorios');
-        const filePath = path.join(dirPath, 'produtos.csv');
-
-        // Verificar se a pasta relatorios existe, se não, criar
-        if (!fs.existsSync(dirPath)) {
-            fs.mkdirSync(dirPath);
-        }
-
-        // Escrever no arquivo CSV
-        fs.writeFile(filePath, csv, (err) => {
-            if (err) {
-                console.error('Erro ao gerar o relatório:', err);
-            } else {
-                console.log('Relatório gerado com sucesso!');
-            }
-        });
+        return this.produtos.find(produto => produto.id === id);
     }
 }
 
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -4,20 +4,20 @@ const fs = require('node:fs');
 class DataService {
     constructor() {
         this.FILE_PATH = path.resolve(__dirname, '..', 'db', 'data.json');
-        this.produtos = this.carregarDados();
+        this.data = this.carregarDados();
     }
 
     // Carregar o arquivo JSON, se não existir, criar um array vazio
     carregarDados() {
         if (!fs.existsSync(this.FILE_PATH)) {
-            fs.writeFileSync(this.FILE_PATH, '[]');
+            fs.writeFileSync(this.FILE_PATH, JSON.stringify({ produtos: [], usuarios: [] }, null, 2));
         }
         return JSON.parse(fs.readFileSync(this.FILE_PATH, 'utf-8'));
     }
 
     // Salvar dados no arquivo JSON
-    salvarDados(produtos) {
-        fs.writeFileSync(this.FILE_PATH, JSON.stringify(produtos, null, 2));
+    salvarDados(data) {
+        fs.writeFileSync(this.FILE_PATH, JSON.stringify(data, null, 2));
     }
 }
 
